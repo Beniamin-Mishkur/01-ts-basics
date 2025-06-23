a460c0a8edbbd2359a2abc184a472978b129d481
Beniamin Mishkur <bmishkur@gmail.com>
2025-06-23 15:53:26 +0200

    hw push

---
+++ src/task-6.ts
@@ -0,0 +1,14 @@
+function getFirstElement<T>(arr: T[]): T {
+    return arr[0];
+  }
+  
+  const firstNumber = getFirstElement<number>([1, 2, 3]);           // number
+  const firstString = getFirstElement<string>(["a", "b", "c"]);     // string
+  const firstBoolean = getFirstElement<boolean>([true, false, true]); // boolean
+  
+  console.log(firstNumber);   // 1
+  console.log(firstString);   // "a"
+  console.log(firstBoolean);  // true
+  
+  getFirstElement([1, "a", true]); 
+  

d01b1ca7447a9904918e0232d0dd11b21247ec78
Beniamin Mishkur <bmishkur@gmail.com>
2025-07-05 17:33:33 +0200

    task-6 fix

--- src/task-6.ts
+++ src/task-6.ts
@@ -4,7 +4,9 @@ function getFirstElement<T>(arr: T[]): T {
   
   const firstNumber = getFirstElement<number>([1, 2, 3]);           // number
   const firstString = getFirstElement<string>(["a", "b", "c"]);     // string
-  const firstBoolean = getFirstElement<boolean>([true, false, true]); // boolean
+const firstBoolean = getFirstElement<boolean>([true, false, true]); // boolean
+  
+const firstMixed = getFirstElement<number | string | boolean>([1, "a", true]);
   
   console.log(firstNumber);   // 1
   console.log(firstString);   // "a"
